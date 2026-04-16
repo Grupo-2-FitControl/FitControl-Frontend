7ada82302ffe401492ef3f08bf47f407e550cb8a
Alberto <bettogp94@gmail.com>
2026-04-16 07:06:02 +0200

    feat: I create Banner component and dd header structure and styles

---
+++ src/components/home/banner/Banner.jsx
@@ -0,0 +1,20 @@
+function Banner() {
+
+  return (
+    <section className="flex flex-col background w-full">
+      <header className="text-center flex flex-col gap-4 lg:flex-row justify-around items-center bg-gray-700 p-4">
+        <div className="  flex flex-col gap-4">
+          <h2 className="text-white text-2xl lg:text-5xl">Actividades Futuras</h2>
+          <p className="text-mist-400 lg:text-2xl">Próximas clases disponibles</p>
+        </div>
+
+        <button className="w-[70%] px-6 py-4 background-primary rounded-md  lg:w-[20%]">
+          +Nueva Actividad
+        </button>
+      </header>
+      
+    </section>
+  );
+}
+
+export default Banner;
--- src/components/home/navbar/Navbar.jsx
+++ src/components/home/navbar/Navbar.jsx
@@ -15,12 +15,9 @@ function Navbar() {
   }, [visible]);
 
   return (
-    <>
+    <header>
       <div className="bg-mist-800 w-full p-4 flex justify-between items-center text-white lg:hidden">
-        <button
-          className="text-2xl"
-          onClick={() => setVisible(true)}
-        >
+        <button className="text-2xl" onClick={() => setVisible(true)}>
           ☰
         </button>
 
@@ -30,17 +27,10 @@ function Navbar() {
         </div>
       </div>
 
-      {/* 🔥 DRAWER (MENÚ COMPLETO) */}
       <aside
-        className={`
-          fixed top-0 left-0 h-full w-72 bg-mist-800 text-white z-50
-          transform transition-transform duration-300
-          ${visible ? "translate-x-0" : "-translate-x-full"}
-
-          lg:translate-x-0 lg:static lg:h-screen
+        className={`fixed top-0 left-0 h-full w-[40%] bg-mist-800 text-white z-50 transform transition-transform duration-300 ${visible ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 lg:static lg:h-screen lg:w-60
         `}
       >
-        {/* HEADER DEL MENÚ */}
         <div className="p-6 flex justify-between items-center border-b border-gray-600">
           <div className="flex items-center gap-2">
             <img src={logo} className="w-10 h-10" />
@@ -55,46 +45,39 @@ function Navbar() {
           </button>
         </div>
 
-        {/* NAV */}
         <nav className="p-4">
-          <ul className="flex flex-col gap-4">
-
-            <li>
-              <Link to="/" onClick={() => setVisible(false)}>
+          <ul className="flex flex-col gap-6 items-center w-full">
+            <li className="text-xl">
+              <Link className="w-full" to="/" onClick={() => setVisible(false)}>
                 Actividades
               </Link>
             </li>
 
-            <li>
+            <li className="text-xl">
               <Link to="/activities" onClick={() => setVisible(false)}>
                 Usuarios
               </Link>
             </li>
 
-            <li>
+            <li className="text-xl">
               <Link to="/teachers" onClick={() => setVisible(false)}>
                 Profesores
               </Link>
             </li>
 
-            <li>
+            <li className="text-xl">
               <Link to="/mis-actividades" onClick={() => setVisible(false)}>
                 Mis actividades
               </Link>
             </li>
-
           </ul>
         </nav>
       </aside>
-
       <div
-        className={`
-          fixed inset-0 bg-black transition-opacity duration-300 z-40
-          ${visible ? "opacity-50" : "opacity-0 pointer-events-none"}
-        `}
+        className={`fixed inset-0 bg-black transition-opacity duration-300 z-40 ${visible ? "opacity-50" : "opacity-0 pointer-events-none"}`}
         onClick={() => setVisible(false)}
       />
-    </>
+    </header>
   );
 }
 
