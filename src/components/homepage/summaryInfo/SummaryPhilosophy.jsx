997954575e1edf7c88da16e66018c5a397bfc101
Alberto <bettogp94@gmail.com>
2026-04-19 17:10:17 +0200

    feat: I create a section with summary philosophy cards grid layout

--- src/components/homepage/summaryInfo/SummaryPhilosophy.jsx
+++ src/components/homepage/summaryInfo/SummaryPhilosophy.jsx
@@ -1,8 +1,8 @@
-function SummaryPhilosophy({title,description,borderColor, textColor}){
+function SummaryPhilosophy({title,description,borderColor, textColorTitle}){
     return(
-        <article className={`p-2 border-3 ${borderColor} text-primary`}>
-            <data className={`text-secondary font-bold text-4xl ${textColor}`} value="8">{title}</data>
-            <p className="text-gray-300">
+        <article className={`flex flex-col justify-center items-center p-6 border-3 ${borderColor} text-primary gap-6`}>
+            <h3 className={`font-bold text-4xl ${textColorTitle} lg:text-6xl`}>{title}</h3>
+            <p className="text-gray-300 uppercase text-center">
                 {description}
             </p>
 
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,7 +4,34 @@ function Homepage() {
   return (
     <article className="flex flex-col background">
       <Banner />
-      
+      <section className="h-screen bg-orange-500">
+        <div className=" grid grid-cols-1 mx-auto p-6 gap-4 md:grid-cols-2 md:px-20 lg:grid-cols-4">
+          <SummaryPhilosophy
+            title={"8+"}
+            description={"Años transformando vidas"}
+            borderColor={"border-[#d4ff00]"}
+            textColorTitle={"text-[#d4ff00]"}
+          />
+          <SummaryPhilosophy
+            title={"6+"}
+            description={"Clases semanales"}
+            borderColor={"border-[#ff5722]"}
+            textColorTitle={"text-[#ff5722]"}
+          />
+          <SummaryPhilosophy
+            title={"200+"}
+            description={"Miembros felices"}
+            borderColor={"border-[#d4ff00]"}
+            textColorTitle={"text-[#d4ff00]"}
+          />
+          <SummaryPhilosophy
+            title={"3"}
+            description={"Instructores Expertos"}
+            borderColor={"border-[#ff5722]"}
+            textColorTitle={"text-[#ff5722]"}
+          />
+        </div>
+      </section>
     </article>
   );
 }
